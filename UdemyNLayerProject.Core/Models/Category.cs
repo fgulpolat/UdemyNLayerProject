23e581e07cf723f599e55a9a5660b9095f426c6e
fgulpolat <49431870+fgulpolat@users.noreply.github.com>
2020-12-10 13:24:55 +0300

    Add project files.

---
+++ UdemyNLayerProject.Core/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyNLayerProject.Core.Models
+{
+   public class Category
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public bool IsDeleted { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
